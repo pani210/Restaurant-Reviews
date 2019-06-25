d4f1f11e04386c273968c855ce91b3ef85bc017b
kodanda pani <51733418+pani210@users.noreply.github.com>
2019-06-25 15:04:30 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,168 +1,187 @@
+let restaurants,
+	neighborhoods,
+	cuisines
+var newMap
+var markers = []
 /**
- * Common database helper functions.
+ * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
-class DBHelper {
-	/**
-	 * Database URL.
-	 * Change this to restaurants.json file location on your server.
-	 */
-	static get DATABASE_URL() {
-		const port = 8000 // Change this to your server port
-		return `http://localhost:${port}/data/restaurants.json`;
-	}
-	/**
-	 * Fetch all restaurants.
-	 */
-	static fetchRestaurants(callback) {
-		let xhr = new XMLHttpRequest();
-		xhr.open('GET', DBHelper.DATABASE_URL);
-		xhr.onload = () => {
-			if (xhr.status === 200) { // Got a success response from server!
-				const json = JSON.parse(xhr.responseText);
-				const restaurants = json.restaurants;
-				callback(null, restaurants);
-			} else { // Oops!. Got an error from server.
-				const error = (`Request failed. Returned status of ${xhr.status}`);
-				callback(error, null);
-			}
-		};
-		xhr.send();
-	}
-	/**
-	 * Fetch a restaurant by its ID.
-	 */
-	static fetchRestaurantById(id, callback) {
-		// fetch all restaurants with proper error handling.
-		DBHelper.fetchRestaurants((error, restaurants) => {
-			if (error) {
-				callback(error, null);
-			} else {
-				const restaurant = restaurants.find(r => r.id == id);
-				if (restaurant) { // Got the restaurant
-					callback(null, restaurant);
-				} else { // Restaurant does not exist in the database
-					callback('Restaurant does not exist', null);
-				}
-			}
-		});
-	}
-	/**
-	 * Fetch restaurants by a cuisine type with proper error handling.
-	 */
-	static fetchRestaurantByCuisine(cuisine, callback) {
-		// Fetch all restaurants  with proper error handling
-		DBHelper.fetchRestaurants((error, restaurants) => {
-			if (error) {
-				callback(error, null);
-			} else {
-				// Filter restaurants to have only given cuisine type
-				const results = restaurants.filter(r => r.cuisine_type == cuisine);
-				callback(null, results);
-			}
-		});
-	}
-	/**
-	 * Fetch restaurants by a neighborhood with proper error handling.
-	 */
-	static fetchRestaurantByNeighborhood(neighborhood, callback) {
-		// Fetch all restaurants
-		DBHelper.fetchRestaurants((error, restaurants) => {
-			if (error) {
-				callback(error, null);
-			} else {
-				// Filter restaurants to have only given neighborhood
-				const results = restaurants.filter(r => r.neighborhood == neighborhood);
-				callback(null, results);
-			}
-		});
-	}
-	/**
-	 * Fetch restaurants by a cuisine and a neighborhood with proper error handling.
-	 */
-	static fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, callback) {
-		// Fetch all restaurants
-		DBHelper.fetchRestaurants((error, restaurants) => {
-			if (error) {
-				callback(error, null);
-			} else {
-				let results = restaurants
-				if (cuisine != 'all') { // filter by cuisine
-					results = results.filter(r => r.cuisine_type == cuisine);
-				}
-				if (neighborhood != 'all') { // filter by neighborhood
-					results = results.filter(r => r.neighborhood == neighborhood);
-				}
-				callback(null, results);
-			}
-		});
-	}
-	/**
-	 * Fetch all neighborhoods with proper error handling.
-	 */
-	static fetchNeighborhoods(callback) {
-		// Fetch all restaurants
-		DBHelper.fetchRestaurants((error, restaurants) => {
-			if (error) {
-				callback(error, null);
-			} else {
-				// Get all neighborhoods from all restaurants
-				const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood)
-				// Remove duplicates from neighborhoods
-				const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) == i)
-				callback(null, uniqueNeighborhoods);
-			}
-		});
-	}
-	/**
-	 * Fetch all cuisines with proper error handling.
-	 */
-	static fetchCuisines(callback) {
-		// Fetch all restaurants
-		DBHelper.fetchRestaurants((error, restaurants) => {
-			if (error) {
-				callback(error, null);
-			} else {
-				// Get all cuisines from all restaurants
-				const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type)
-				// Remove duplicates from cuisines
-				const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) == i)
-				callback(null, uniqueCuisines);
-			}
-		});
-	}
-	/**
-	 * Restaurant page URL.
-	 */
-	static urlForRestaurant(restaurant) {
-		return (`./restaurant.html?id=${restaurant.id}`);
-	}
-	/**
-	 * Restaurant image URL.
-	 */
-	static imageUrlForRestaurant(restaurant) {
-		return (`/img/${restaurant.photograph}`);
-	}
-	/**
-	 * Map marker for a restaurant.
-	 */
-	static mapMarkerForRestaurant(restaurant, map) {
-		// https://leafletjs.com/reference-1.3.0.html#marker
-		const marker = new L.marker([restaurant.latlng.lat, restaurant.latlng.lng], {
-			title: restaurant.name,
-			alt: restaurant.name,
-			url: DBHelper.urlForRestaurant(restaurant)
-		})
-		marker.addTo(newMap);
-		return marker;
-	}
-	/* static mapMarkerForRestaurant(restaurant, map) {
-	  const marker = new google.maps.Marker({
-	    position: restaurant.latlng,
-	    title: restaurant.name,
-	    url: DBHelper.urlForRestaurant(restaurant),
-	    map: map,
-	    animation: google.maps.Animation.DROP}
-	  );
-	  return marker;
-	} */
+document.addEventListener('DOMContentLoaded', (event) => {
+	initMap(); // added
+	fetchNeighborhoods();
+	fetchCuisines();
+});
+/**
+ * Fetch all neighborhoods and set their HTML.
+ */
+fetchNeighborhoods = () => {
+	DBHelper.fetchNeighborhoods((error, neighborhoods) => {
+		if (error) { // Got an error
+			console.error(error);
+		} else {
+			self.neighborhoods = neighborhoods;
+			fillNeighborhoodsHTML();
+		}
+	});
 }
+/**
+ * Set neighborhoods HTML.
+ */
+fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
+	const select = document.getElementById('neighborhoods-select');
+	neighborhoods.forEach(neighborhood => {
+		const option = document.createElement('option');
+		option.innerHTML = neighborhood;
+		option.value = neighborhood;
+		select.append(option);
+	});
+}
+/**
+ * Fetch all cuisines and set their HTML.
+ */
+fetchCuisines = () => {
+	DBHelper.fetchCuisines((error, cuisines) => {
+		if (error) { // Got an error!
+			console.error(error);
+		} else {
+			self.cuisines = cuisines;
+			fillCuisinesHTML();
+		}
+	});
+}
+/**
+ * Set cuisines HTML.
+ */
+fillCuisinesHTML = (cuisines = self.cuisines) => {
+	const select = document.getElementById('cuisines-select');
+	cuisines.forEach(cuisine => {
+		const option = document.createElement('option');
+		option.innerHTML = cuisine;
+		option.value = cuisine;
+		select.append(option);
+	});
+}
+/**
+ * Initialize leaflet map, called from HTML.
+ */
+initMap = () => {
+	self.newMap = L.map('map', {
+		center: [40.722216, -73.987501],
+		zoom: 12,
+		scrollWheelZoom: false
+	});
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+		mapboxToken: 'pk.eyJ1IjoicGFuaTIxMCIsImEiOiJjang5ejY3ZTYwMDNwM3lwOWJtdjd4Y2s0In0.gt82tD-jjQqo1dgr5LjvjA',
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' + '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(newMap);
+	updateRestaurants();
+}
+/* window.initMap = () => {
+  let loc = {
+    lat: 40.722216,
+    lng: -73.987501
+  };
+  self.map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: loc,
+    scrollwheel: false
+  });
+  updateRestaurants();
+} */
+/**
+ * Update page and map for current restaurants.
+ */
+updateRestaurants = () => {
+	const cSelect = document.getElementById('cuisines-select');
+	const nSelect = document.getElementById('neighborhoods-select');
+	const cIndex = cSelect.selectedIndex;
+	const nIndex = nSelect.selectedIndex;
+	const cuisine = cSelect[cIndex].value;
+	const neighborhood = nSelect[nIndex].value;
+	DBHelper.fetchRestaurantByCuisineAndNeighborhood(cuisine, neighborhood, (error, restaurants) => {
+		if (error) { // Got an error!
+			console.error(error);
+		} else {
+			resetRestaurants(restaurants);
+			fillRestaurantsHTML();
+		}
+	})
+}
+/**
+ * Clear current restaurants, their HTML and remove their map markers.
+ */
+resetRestaurants = (restaurants) => {
+	// Remove all restaurants
+	self.restaurants = [];
+	const ul = document.getElementById('restaurants-list');
+	ul.innerHTML = '';
+	// Remove all map markers
+	if (self.markers) {
+		self.markers.forEach(marker => marker.remove());
+	}
+	self.markers = [];
+	self.restaurants = restaurants;
+}
+/**
+ * Create all restaurants HTML and add them to the webpage.
+ */
+fillRestaurantsHTML = (restaurants = self.restaurants) => {
+	const ul = document.getElementById('restaurants-list');
+	restaurants.forEach(restaurant => {
+		ul.append(createRestaurantHTML(restaurant));
+	});
+	addMarkersToMap();
+}
+/**
+ * Create restaurant HTML.
+ */
+createRestaurantHTML = (restaurant) => {
+	const li = document.createElement('li');
+	const image = document.createElement('img');
+	image.className = 'restaurant-img';
+	image.src = "." + DBHelper.imageUrlForRestaurant(restaurant);
+	image.alt = restaurant.name;
+	li.append(image);
+	image.alt = restaurant.name;
+	const name = document.createElement('h1');
+	name.innerHTML = restaurant.name;
+	li.append(name);
+	const neighborhood = document.createElement('p');
+	neighborhood.innerHTML = restaurant.neighborhood;
+	li.append(neighborhood);
+	const address = document.createElement('p');
+	address.innerHTML = restaurant.address;
+	li.append(address);
+	const more = document.createElement('a');
+	more.innerHTML = 'View Details';
+	more.href = DBHelper.urlForRestaurant(restaurant);
+	li.append(more)
+	return li
+}
+/**
+ * Add markers for current restaurants to the map.
+ */
+addMarkersToMap = (restaurants = self.restaurants) => {
+	restaurants.forEach(restaurant => {
+		// Add marker to the map
+		const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
+		marker.on("click", onClick);
+
+		function onClick() {
+			window.location.href = marker.options.url;
+		}
+		self.markers.push(marker);
+	});
+}
+/* addMarkersToMap = (restaurants = self.restaurants) => {
+  restaurants.forEach(restaurant => {
+    // Add marker to the map
+    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
+    google.maps.event.addListener(marker, 'click', () => {
+      window.location.href = marker.url
+    });
+    self.markers.push(marker);
+  });
+} */
